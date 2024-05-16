42c060198e6e5c637771aed758d5b7a233225fc1
Darvid-223 <david.arvidsson@hotmail.com>
2024-05-16 19:07:48 +0200

    import modules and ascii art

---
+++ run.py
@@ -0,0 +1,10 @@
+"""
+Import randint from random to generate bot coordinates.
+Import os to clear the terminal.
+Import ascii_art to display ASCII art.
+"""
+from random import randint
+import os
+import ascii_art
+
+
